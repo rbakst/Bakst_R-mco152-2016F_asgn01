16357428b46edd832fe04376e5aa5b3663eaeccc
Rachel <rbakst@student.touro.edu>
2016-09-15 16:18:36 -0400

    Modified to add 2 numbers

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String [] args)
 	{
-		//this line displays a message
-		//and another one
-		System.out.println("Hello world");
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Please enter a number: ");
+		int num1 = keyboard.nextInt();
+		
+		System.out.println("Please enter another number: ");
+		int num2 = keyboard.nextInt();
+		
+		int sum = num1 + num2;
+		
+		System.out.println("Sum: " + sum);
 	}
 }
